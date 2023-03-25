51fb75d9eb4882ae28f8891308be4e53d5f43546
GYPM4H <zenomonster@gmail.com>
2023-03-25 16:47:34 +0200

    implemented and fixed election algorithm

--- node.py
+++ node.py
@@ -15,30 +15,30 @@ class TicTacToeServicer(node_pb2_grpc.TicTacToeServicer):
         self.node_id = node_id
 
     def StartGame(self, request, context):
+        #retrive current state of node
         n_nodes = request.n_nodes
         node_id = request.node_id
         node_time = request.node_time
         isLeader = request.isLeader
         ids = request.list_ids
-
+        #if node port in list -> loop is completed and we can select a leader
         if node_id in ids:
             print("Ring is complete. Choosing leader ....")
             leader_id = max(ids)
+            print(f"Leader is on port {leader_id}")
             isLeader = True
             leader_time = datetime.datetime.utcnow().isoformat()
-            n_players = 2
-            for i in range(n_players):
-                with grpc.insecure_channel(f"localhost:{(leader_id + i - BASE_PORT + 1) % n_nodes + BASE_PORT}"):
-                     return node_pb2.ElectionResponse(leader_id= leader_id, isComplete = isLeader, leader_time= leader_time)
-        
+            return node_pb2.ElectionResponse(leader_id= leader_id, isComplete = isLeader, leader_time= leader_time)
+        #if not -> we append the port of the current node to the list. Pass this list to the next node.
         if node_id not in ids:
             ids.append(node_id)
             isLeader = False
             print("Collected current ID. Moving to the next node ....")
             next_node_id = (node_id - BASE_PORT + 1) % n_nodes + BASE_PORT
+            #connecting to the next node, pass the list of ids
             with grpc.insecure_channel(f"localhost:{next_node_id}") as channel:
-                stub = node_pb2_grpc.RingElectionStub(channel)
-                response = stub.Election(node_pb2.InitParams(n_nodes= n_nodes, 
+                stub = node_pb2_grpc.TicTacToeStub(channel)
+                response = stub.StartGame(node_pb2.InitParams(n_nodes= n_nodes, 
                                                              node_id= next_node_id, 
                                                              node_time= node_time, 
                                                              isLeader= isLeader,
@@ -52,15 +52,21 @@ class Main(cmd.Cmd):
         super().__init__(*args, **kwargs)
         self.node_id = node_id
         self.prompt = f'Node-{node_id}> '
+        self.node_port = BASE_PORT + node_id
 
         self.channel = grpc.insecure_channel(f"localhost:{BASE_PORT + 1}")
         self.stub = node_pb2_grpc.TicTacToeStub(self.channel)
 
     def do_Start_game(self, args):
-        with grpc.insecure_channel(f"localhost:{BASE_PORT + 1}") as channel:
+        #After we Start_game, connect to the BASE_PORT(50060)(to yourself) and initiate election
+        with grpc.insecure_channel(f"localhost:{self.node_port}") as channel:
             stub = node_pb2_grpc.TicTacToeStub(channel)
-            request = node_pb2.InitParams(n_nodes=0, node_id=0, node_time='', isLeader=True, list_ids=[])
+            request = node_pb2.InitParams(n_nodes=3, node_id=self.node_port, node_time='', isLeader=False, list_ids=[])
             response = stub.StartGame(request)
+            #When the circle is completed, present a new leader
+            if response.isComplete == True:
+                print(f"Now I know that the leader is {response.leader_id}")
+                print(f"Leader time is {response.leader_time}")
             print("Game started")
             return response
 
@@ -78,6 +84,7 @@ class Main(cmd.Cmd):
 
 
 if __name__ == '__main__':
+    #Creating 3 nodes with ports 50060 50061 50062
     node_id = int(sys.argv[1])
     run_port = BASE_PORT + node_id
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
